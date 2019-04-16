1b07476cfced06efe226489ca89130b645230074
saleem143 <46808594+saleem143@users.noreply.github.com>
2019-04-15 19:51:38 -0700

    Update app-routing.module.ts
    extension 1

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmplyoeeDetailsComponent } from './emplyoee-details/emplyoee-details.component';
+import { DepartmentComponent } from './department/department.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'',component:EmplyoeeDetailsComponent},//for handling defalut route its means localhost:4200 is given then redirect to #/empdetails
+  { path:'empdetalis',component:EmplyoeeDetailsComponent},
+  { path:'emplist',component:EmployeeListComponent},
+  { path:'department',component:DepartmentComponent},
+ // { path:'', redirectTo:'/empdetails' pathMatch:'full'},
+  { path:'**', component:PagenotfoundComponent}//should last row of this section
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents =[EmployeeListComponent,EmplyoeeDetailsComponent,DepartmentComponent,PagenotfoundComponent]
